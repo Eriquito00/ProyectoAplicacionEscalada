6921976d0b195a72ebfd7626155c1ff8e98070b9
Eriquito00 <ericmejiasgamonal@gmail.com>
2025-04-01 11:57:52 +0200

    Creacion de funciones base y plantilla de controlador

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,4 +1,49 @@
 package controller;
 
+import view.View;
+
+import java.util.Scanner;
+
 public class Main {
+    public static Scanner scan = new Scanner(System.in);
+    public static void main(String[] args){
+        boolean seguir = true;
+
+        View.mostrarMsg("Bienvenido al gestionador de Vias de Escalada!");
+
+        while (seguir){
+
+            View.mostrarMenu("A","B","C","D");
+            int opcion = aplicaOpcio(scan, 1, 4);
+
+            switch (opcion){
+                case 1:
+                    View.mostrarMsg("1");
+                    break;
+                case 2:
+                    View.mostrarMsg("2");
+                    break;
+                case 3:
+                    View.mostrarMsg("3");
+                    break;
+                case 4:
+                    View.mostrarMsg("salir");
+                    seguir = false;
+                    break;
+            }
+        }
+    }
+    public static int aplicaOpcio(Scanner s, int min, int max){
+        int n = 0;
+
+        try {
+            n = Integer.parseInt(s.nextLine());
+            if (n < min || n > max) throw new NumberFormatException();
+        }
+        catch (NumberFormatException e){
+            View.mostrarMsg("El valor introducido no es valido entre les opciones.");
+        }
+
+        return n;
+    }
 }
--- src/view/View.java
+++ src/view/View.java
@@ -1,4 +1,14 @@
 package view;
 
 public class View {
+    public static void mostrarMsg(String msg){
+        System.out.println(msg);
+    }
+
+    public static void mostrarMenu(String ... opciones){
+        System.out.println("Introdueix una de les opcions seguents:");
+        for (int i = 1; i <= opciones.length; i++){
+            System.out.println(i + ": " + opciones[i - 1]);
+        }
+    }
 }
