b122cd9aeea608b0a863fd05b3acccfb9e2292f8
Wysper <wysperotaku@gmail.com>
2025-04-11 10:12:13 +0200

    testing vies

--- src/utils/Test.java
+++ src/utils/Test.java
@@ -26,6 +26,29 @@ public class Test {
         }
     }
 
+    public static void pruebaVies(Connection c) throws SQLException {
+        Statement s = c.createStatement();
+        ResultSet rs = s.executeQuery("Select * from vies");
+        ResultSetMetaData md = rs.getMetaData();
+
+        String col = String.format("%-30s %-5s %-5s %-5s %-10s",
+                md.getColumnName(8),
+                md.getColumnName(9),
+                md.getColumnName(10),
+                md.getColumnName(11),
+                md.getColumnName(12));
+        System.out.println(col + "\n");
+        while (rs.next()){
+            String str = String.format("%-30s %-5s %-5s %-5s %-10s",
+                    rs.getString(8),
+                    rs.getString(9),
+                    rs.getString(10),
+                    rs.getString(11),
+                    rs.getString(12));
+            System.out.println(str + "\n");
+        }
+    }
+
     public static void pruebaSectores(Connection c) throws SQLException {
         Statement s = c.createStatement();
         ResultSet rs = s.executeQuery("Select * from sectors");
